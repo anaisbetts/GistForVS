d9a48a44c613622ba6e7ae38d3d4bd1ec7c8e11d
Paul Betts <paul@paulbetts.org>
2011-08-24 18:38:47 -0700

    Add test that previews the UI

--- GistForVS.Tests/Scratch.cs
+++ GistForVS.Tests/Scratch.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using GistForVS.Tests.TestHelpers;
+using GistForVS.Views;
 using Xunit;
 
 namespace GistForVS.Tests
@@ -12,6 +16,36 @@ namespace GistForVS.Tests
         public void TestFail()
         {
             Assert.True(false);
+        }
+
+        [Fact]
+        public void CreateTestWindow()
+        {
+            Application.ResourceAssembly = typeof (InsertGistControl).Assembly;
+
+            WpfHelper.RunBlockAsSTA(() => {
+                var grid = new Grid();
+                var lhs = new ColumnDefinition() {Width = new GridLength(0.5, GridUnitType.Star)};
+                var rhs = new ColumnDefinition() {Width = new GridLength(0.5, GridUnitType.Star)};
+                grid.ColumnDefinitions.Add(lhs);    grid.ColumnDefinitions.Add(rhs);
+
+                var textBox = new TextBox();
+                var control = new InsertGistControl() {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+
+                Grid.SetColumn(textBox, 0);
+                Grid.SetColumn(control, 1);
+                textBox.TextChanged += (o, e) => control.ViewModel.SelectionText = textBox.Text;
+                grid.Children.Add(textBox); grid.Children.Add(control);
+
+                var wnd = new Window() {
+                    Content = grid,
+                };
+
+                wnd.ShowDialog();
+            });
         }
     }
 }
