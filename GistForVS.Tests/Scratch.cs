86b8fe81dabebc412a9f661640cda8e7767cc6b7
Paul Betts <paul@paulbetts.org>
2011-08-24 17:04:15 -0700

    Add Test project

---
+++ GistForVS.Tests/Scratch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace GistForVS.Tests
+{
+    public class Scratch
+    {
+        [Fact]
+        public void TestFail()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ GistForVS.Tests/TestHelpers/WpfHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GistForVS.Tests.TestHelpers
+{
+    public class WpfHelper
+    {
+        public static void RunBlockAsSTA(Action block)
+        {
+            Exception ex = null;
+            var t = new Thread(() =>
+            {
+                try
+                {
+                    block();
+                }
+                catch(Exception e)
+                {
+                    ex = e;
+                    throw;
+                }
+            });
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+
+            if (ex != null)
+            {
+                // NB: If we don't do this, the test silently passes
+                throw ex;
+            }
+        }
+    }
+}
